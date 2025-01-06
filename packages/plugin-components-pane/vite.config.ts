0c87f3d70cf59c7db08214c53b5a066f29b9a8ac
mazhaobo <mazhaobo@hoteamsoft.com>
2025-01-06 22:02:54 +0800

    build(plugin-components-pane): 重构构建配置并升级依赖
    - 移除 .eslintignore 和 .eslintrc.js 文件
    - 更新 package.json，包括版本号、文件结构和依赖项
    - 重命名 src/index.tsx 为 src/index.ts
    - 新增 vite.config.ts 配置 Vite 构建

---
+++ packages/plugin-components-pane/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import React from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import external from 'vite-plugin-external';
+
+export default defineConfig({
+  plugins: [
+    // external({
+    //   externals: {
+    //     react: 'React',
+    //   },
+    // }),
+    React({}),
+    dts(),
+  ],
+  server: {
+    port: 4173,
+    hmr: false,
+  },
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      fileName(format, entryName) {
+        return `${entryName}.${format}.js`;
+      },
+      name: 'LowCodePluginComponentsPane',
+      cssFileName: 'index',
+    },
+    rollupOptions: {
+      output: {
+        exports: 'named',
+      },
+      external: ['react', 'react-dom'],
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // https://cn.vitejs.dev/config/shared-options.html#css-preprocessoroptions
+        api: 'legacy',
+      },
+    },
+  },
+});

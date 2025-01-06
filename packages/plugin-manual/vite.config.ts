dc02fcf932b01bdb2ff7401e3333a7fb66047ba8
mazhaobo <mazhaobo@hoteamsoft.com>
2025-01-06 14:02:35 +0800

    build(plugin-manual): 重构构建配置并迁移至 Vite
    - 移除 build.json 和 build.plugin.js 文件
    - 更新 package.json 中的构建脚本和依赖
    - 新增 vite.config.ts 文件配置 Vite 构建
    - 调整项目结构，统一入口文件路径

---
+++ packages/plugin-manual/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import React from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import external from 'vite-plugin-external';
+
+export default defineConfig({
+  plugins: [
+    // external({
+    //   externals: {
+    //     react: 'React',
+    //   },
+    // }),
+    React({}),
+    dts(),
+  ],
+  server: {
+    port: 4173,
+    hmr: false,
+  },
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      fileName(format, entryName) {
+        return `${entryName}.${format}.js`;
+      },
+      name: 'LowCodePluginSchema',
+      cssFileName: 'index',
+    },
+    rollupOptions: {
+      output: {
+        exports: 'named',
+      },
+      external: [],
+    },
+  },
+});
